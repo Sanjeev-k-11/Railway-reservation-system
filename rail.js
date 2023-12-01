eed8f2b121f863aebd285535ba16464d6a35d370
sanjeev kumar yadav <148857871+Sanjeev-k-11@users.noreply.github.com>
2023-12-01 20:54:29 +0530

    done

---
+++ rail.js
@@ -0,0 +1,23 @@
+// function bookTicket() {
+//   // Get class values
+//   var trainName = document.getElementById("trainName").value;
+//   var source = document.getElementById("source").value;
+//   var destination = document.getElementById("destination").value;
+//   var departureDate = document.getElementById("departureDate").value;
+//   var passengerName = document.getElementById("passengerName").value;
+//   var numOfTickets = document.getElementById("numOfTickets").value;
+
+//   // Display booking status
+//   var bookingStatus = document.getElementById("bookingStatus");
+//   bookingStatus.innerHTML = `
+//     <h2>Booking Status</h2>
+//     <p>Train Name: ${trainName}</p>
+//     <p>Source: ${source}</p>
+//     <p>Destination: ${destination}</p>
+//     <p>Departure Date: ${departureDate}</p>
+//     <p>Passenger Name: ${passengerName}</p>
+//     <p>Number of Tickets: ${numOfTickets}</p>
+//     <p>Booking Successful!</p>
+//   `;
+// }
+
